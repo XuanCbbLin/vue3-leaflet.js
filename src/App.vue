bbe02f55f53aecc104224b248f139d401d77bed5
xuan <xuancbblin@gmail.com>
2022-08-03 19:06:51 +0800

    feat: 新增icon 圖片

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <!-- <Day1 /> -->
-  <Day2 />
+  <!-- <Day2 /> -->
+  <Day3 />
 </template>
 
 <script setup>
 import Day1 from "./docs/day1/Day1.vue";
 import Day2 from "./docs/day2/Day2.vue";
+import Day3 from "./docs/day3/Day3.vue";
 </script>
 
 <style lang="scss" scoped></style>
