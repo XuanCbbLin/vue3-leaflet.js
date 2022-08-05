7fb1ef22db4280c5d474399a2c380bd8aca602b3
xuan <xuancbblin@gmail.com>
2022-08-05 18:03:26 +0800

    feat: add  Interactive Choropleth Map

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <!-- <Day1 /> -->
   <!-- <Day2 /> -->
   <!-- <Day3 /> -->
-  <Day4 />
+  <!-- <Day4 /> -->
+  <Day5 />
 </template>
 
 <script setup>
@@ -10,6 +11,7 @@ import Day1 from "./docs/day1/Day1.vue";
 import Day2 from "./docs/day2/Day2.vue";
 import Day3 from "./docs/day3/Day3.vue";
 import Day4 from "./docs/day4/Day4.vue";
+import Day5 from "./docs/day5/Day5.vue";
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/docs/day5/Day5.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mapContainer" ref="mapContainer"></div>
+</template>
+
+<script setup>
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { onMounted, ref } from "vue";
+import { statesData } from "../day5/us-states";
+
+const mapContainer = ref(null);
+
+onMounted(() => {
+  var map = L.map(mapContainer.value).setView([37.8, -96], 4);
+
+  var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  /* global statesData */
+  var geojson = L.geoJson(statesData).addTo(map);
+});
+</script>
+
+<style lang="scss" scoped>
+.mapContainer {
+  height: 600px;
+}
+</style>
