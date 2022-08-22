c666b51be01459d25833340c204821a5d1b83b16
xuan <xuancbblin@gmail.com>
2022-08-22 19:01:26 +0800

    feat: 新增leaflet.markercluster

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <Panes />
+  <!-- <Panes /> -->
+  <MarkerCluster />
 </template>
 
 <script setup>
 import Panes from "./docs/day9/Panes.vue";
 import Day4 from "./docs/day4/Day4.vue";
+import MarkerCluster from "./docs/day12/MarkerCluster.vue";
 </script>
 
 <style lang="scss" scoped></style>
